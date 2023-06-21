36b4f7e358f0bc388f15bc986c39abfd2a77d117
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-06-21 20:11:01 +0100

    feat: automatically decide how to reboot

--- pkg/reboot/reboot_linux.go
+++ pkg/reboot/reboot_linux.go
@@ -1,11 +1,74 @@
 package reboot
 
-import "syscall"
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"go.uber.org/multierr"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+const graceTime = time.Second * 15
 
 type DefaultRebootImpl struct {
 }
 
-func (l *DefaultRebootImpl) Reboot() error {
-	syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART)
+func (l *DefaultRebootImpl) Reboot() (err error) {
+	defer func(err error) {
+		if err == nil {
+			// Give some time to system to actually reboot
+			time.Sleep(graceTime)
+		}
+	}(err)
+
+	uid := os.Getuid()
+	if uid == 0 {
+		err = rebootAsRoot()
+	} else {
+		err = rebootAsUser()
+	}
+
+	return
+}
+
+func rebootAsRoot() error {
+	var errs error
+
+	log.Info().Msg("Running as root, attempting direct reboot...")
+	if err := syscall.Reboot(syscall.LINUX_REBOOT_CMD_RESTART); err != nil {
+		errs = multierr.Append(errs, fmt.Errorf("reboot via syscall did not work: %w", err))
+	} else {
+		return nil
+	}
+
+	log.Info().Msg("Running as root, attempting reboot via systemctl reboot ...")
+	cmd := exec.Command("systemctl", "reboot")
+	if err := cmd.Run(); err != nil {
+		errs = multierr.Append(errs, fmt.Errorf("reboot via 'systemctl reboot' did not work: %w", err))
+		return errs
+	}
+
+	return nil
+}
+
+func rebootAsUser() error {
+	log.Info().Msg("Not running as root, trying rebooting the system via 'sudo systemctl reboot'... ")
+
+	var errs error
+	cmd := exec.Command("sudo", "systemctl", "reboot")
+	if err := cmd.Run(); err != nil {
+		errs = multierr.Append(errs, fmt.Errorf("reboot system using 'sudo systemctl reboot' did not work: %w", err))
+	} else {
+		return nil
+	}
+
+	cmd = exec.Command("sudo", "reboot")
+	if err := cmd.Run(); err != nil {
+		errs = multierr.Append(errs, fmt.Errorf("reboot system using 'sudo reboot' did not work: %w", err))
+		return errs
+	}
+
 	return nil
 }
